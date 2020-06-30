e4ca9d359b1a2456a8cafd805bf2def7b1d1d538
johnseabi <seabi.tkoregur@gmail.com>
2020-06-30 14:54:30 +0200

    initial commit

--- AccessAzureDB/AccessAzureDB/MainPage.xaml.cs
+++ AccessAzureDB/AccessAzureDB/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.Data.SqlClient;
+using Android.Widget;
 
 namespace AccessAzureDB
 {
@@ -13,9 +15,73 @@ namespace AccessAzureDB
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        static List<PoorPerformingStudent> listOfStudents = new List<PoorPerformingStudent>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            listOfStudents = Getstudents();
+            //Sample code
+            list.FindByName<Xamarin.Forms.ListView>("list");
+
+            list.ItemsSource = listOfStudents;
+        }
+
+        partial class PoorPerformingStudent
+        {
+            public PoorPerformingStudent(string studentNumber, string fullName, string module, string campus, string phone, string contactMode)
+            {
+                StudentNumber = studentNumber;
+                FullName = fullName;
+                Module = module;
+                Campus = campus;
+                Phone = phone;
+                ContactMode = contactMode;
+            }
+
+            public string StudentNumber { get; set; }
+            public string FullName { get; set; }
+            public string Module { get; set; }
+            public string Campus { get; set; }
+            public string Phone { get; set; }
+            public string ContactMode { get; set; }
+        }
+        static List<PoorPerformingStudent> Getstudents()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "prj381server.database.windows.net";
+                builder.UserID = "john";
+                builder.Password = "StudentAnalyser123";
+                builder.InitialCatalog = "TestDB";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT TOP (1000) [StudentNumber],[FullNames],[Modules],[Campus] ,[ContactNumber],[ContactMode]FROM[dbo].[PoorPerformingStudent]");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                listOfStudents.Add(new PoorPerformingStudent(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5)));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+
+            }
+            return listOfStudents;
         }
     }
 }
